3e755870b79a2652efa14cd26e420f145fb4d5a3
Sasi-007 <rahd006@gmail.com>
2019-12-09 23:30:04 +0530

    changes made

--- index.php
+++ index.php
@@ -48,35 +48,10 @@
 		 <div class="topnav-right">
 			<a href="#search" class="active">Employee Login</a>
 			&nbsp;
-			<a href="#about">Admin Login</a>
+			<a href="adminlogin.php">Admin Login</a>
 		 </div>
 	  </div>
-	<div class="w3-main" style="margin-left:0px">
-		<div class="w3-hide-large" style="margin-top:83px"></div>	  
-       <div class ="container-fluid" style="padding:5% ;padding-left:13.5%" >
-                <div class="row">
-                    <div class="col-sm-3">
-                    </div>
-                    <div class="col-sm-4 "  style="padding-top:2.5%">
-                            <form action="login.php" method="post">
-                                 
-                                <div class="form-group">
-                                         <label for="ID NO"><strong>Employee Email ID</strong></label><input type="email"  name="email" class="form-control"  required/>
-                                </div>
-                                <div class="form-group">
-                                         <label for="password"><strong>Password</strong></label><input type="password"  id="shwpwd" name="password" class="form-control"  required/>
-                                </div>
-                                <div class="form-group">
-                                         <input type="checkbox"   onclick="shw()"/><label for="password">&nbsp;<strong>Show Password</strong></label>
-                                </div>
-                                <div class="form-group">
-                                         <input type="submit"  name="submit" class="form-control btn btn-primary" required/>
-                                </div>
-                            </form>
-                    </div>
-                </div>
-        </div>
-	</div>
+	
 	
 </body>
 </html>
